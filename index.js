c69f4c19b4988982d21453e63e87d54d1a280249
Yusuff Mustapha <mustaphee94@gmail.com>
2021-04-04 18:27:01 +0100

    chore: improve the code
    - format the code properly
    - change the default response message

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-module.exports = function(customResponse) {
+module.exports = function (customResponse) {
   return function (error, req, res, next) {
     if (error instanceof SyntaxError) {
-      const defaultErrorMessage = { status: 'fail',
-      message: 'The server is unable to process your request as it is badly malformed!' }
-      return res.status(400).send(customResponse || defaultErrorMessage );
-    } else {
-      next();
+      const defaultErrorMessage = {
+        status: 'fail',
+        message: 'Invalid JSON: The server is unable to process your request as it is badly malformed!',
+      };
+      return res.status(400).send(customResponse || defaultErrorMessage);
     }
-  }
-}
+    return next();
+  };
+};
